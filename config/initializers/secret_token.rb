202182fe78c9ed2178a41c4948e1b4a9aba86ffd
Максим Мясников <gringo.maxim@gmail.com>
2014-04-15 21:13:22 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Vcard::Application.config.secret_key_base = 'f7c3cba3e07c62d255d49ccb778497feab2a363e2dce0c7f6b20c1330ba29cc5ad7eb6ac8c41241c91ee115a9a553a190d9edb74667cb849d9f4f13286c47215'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Vcard::Application.config.session_store :cookie_store, key: '_vcard_session'
